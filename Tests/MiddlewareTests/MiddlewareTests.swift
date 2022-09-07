b2dd2f8fd6b6c7e38e348fd5c613afb094c84169
Carlos Diaz <carlos.diaz@ymedialabs.com>
2022-09-07 18:11:15 +0200

    initial commit

---
+++ Tests/MiddlewareTests/MiddlewareTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import Middleware
+
+final class MiddlewareTests: XCTestCase {
+    func testExample() throws {
+
+    }
+}
--- Tests/SampleDataSourceTests/SampleDataSourceTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import SampleDataSource
-
-final class SampleDataSourceTests: XCTestCase {
-    func testExample() throws {
-
-    }
-}
